eb476c08dc3b302be7a76379b5f695d92f35f06c
Demurus <demurus@gmail.com>
2026-09-24 14:13:56 +0200

    Main models + price calculation

---
+++ Entities/Booking.cs
@@ -0,0 +1,60 @@
+﻿
+namespace IlliaUlianych_APB_TZ.Entities;
+
+public class Booking
+{
+    private readonly List<RoomService> _selectedServices = new();
+
+    public int Id { get; private set; }
+
+    public int RoomId { get; private set; }
+    public ConferenceRoom Room { get; private set; } = null!;
+
+    public DateTime StartTime { get; private set; }
+    public DateTime EndTime { get; private set; }
+
+    public decimal TotalPrice { get; private set; }
+
+    public IReadOnlyCollection<RoomService> SelectedServices =>
+        _selectedServices;
+
+    private Booking()
+    {
+    }
+
+    public Booking(
+        ConferenceRoom room,
+        DateTime startTime,
+        TimeSpan duration,
+        decimal totalPrice,
+        IEnumerable<RoomService>? selectedServices = null)
+    {
+        ArgumentNullException.ThrowIfNull(room);
+
+        if (duration <= TimeSpan.Zero)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(duration),
+                "Booking duration must be greater than zero.");
+        }
+
+        if (totalPrice < 0)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(totalPrice),
+                "Total price cannot be negative.");
+        }
+
+        Room = room;
+        RoomId = room.Id;
+
+        StartTime = startTime;
+        EndTime = startTime.Add(duration);
+        TotalPrice = totalPrice;
+
+        if (selectedServices is not null)
+        {
+            _selectedServices.AddRange(selectedServices);
+        }
+    }
+}
---
+++ Entities/ConferenceRoom.cs
@@ -0,0 +1,53 @@
+﻿namespace IlliaUlianych_APB_TZ.Entities;
+
+public class ConferenceRoom
+{
+    private readonly List<RoomService> _availableServices = new List<RoomService>();
+    
+    public int Id { get; private set; }
+    public string Name { get; private set; } = null!;
+    public int Capacity { get; private set; }
+    public decimal BaseHourPrice { get; private set; }
+    public IReadOnlyCollection<RoomService> AvailableServices => _availableServices;
+
+    private ConferenceRoom()
+    {
+    }
+
+    public ConferenceRoom(
+        string name,
+        int capacity,
+        decimal baseHourPrice,
+        IEnumerable<RoomService>? availableServices = null)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            throw new ArgumentException(
+                "Name cannot be null or empty.",
+                nameof(name));
+        }
+
+        if (capacity <= 0)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(capacity),
+                "Capacity must be greater than zero.");
+        }
+
+        if (baseHourPrice < 0)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(baseHourPrice),
+                "Price cannot be negative.");
+        }
+
+        Name = name;
+        Capacity = capacity;
+        BaseHourPrice = baseHourPrice;
+
+        if (availableServices is not null)
+        {
+            _availableServices.AddRange(availableServices);
+        }
+    }
+}
